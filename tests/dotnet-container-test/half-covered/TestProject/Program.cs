9e0f8887c2dd5e4ea9a39a7657beec0c6c454a2a
Victor <2417328+vrobinson@users.noreply.github.com>
2025-05-08 20:50:48 +0000

    Align code with assertion

--- tests/dotnet-container-test/half-covered/TestProject/Program.cs
+++ tests/dotnet-container-test/half-covered/TestProject/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello dotnet-container-test");
+        Console.WriteLine("Hello from dotnet-container");
     }
 
     public static string Covered(string input)
