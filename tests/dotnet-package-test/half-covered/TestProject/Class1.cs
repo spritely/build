c70d32da259cbf462b66f52c2d67b6a55584d941
Victor <2417328+vrobinson@users.noreply.github.com>
2025-05-08 22:24:18 +0000

    Add test for dotnet-package

---
+++ tests/dotnet-package-test/half-covered/TestProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace TestProject;
+
+public static class Class1
+{
+    public static string Covered(string input)
+    {
+        return input;
+    }
+
+    public static string Uncovered(string input)
+    {
+        return input;
+    }
+}
