2729c421a5cde9e46b20e2f3c61f5af3322414a0
Victor <2417328+vrobinson@users.noreply.github.com>
2025-05-08 20:07:12 +0000

    Add dotnet-container-test

---
+++ tests/dotnet-container-test/half-covered/TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestProject;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello dotnet-container-test");
+    }
+
+    public static string Covered(string input)
+    {
+        return input;
+    }
+}
